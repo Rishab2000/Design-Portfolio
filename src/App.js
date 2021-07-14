9b8e700a3f9fe0c58eaaee982da4fe80a90f66a2
Rishab2000 <rishabsachidanand@gmail.com>
2021-07-14 21:40:45 +0530

    Start tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header><h1>Rishab Sachidanand</h1></header>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+        Button
+      </button>
+
     </div>
   );
 }
