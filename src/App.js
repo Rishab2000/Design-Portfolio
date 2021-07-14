61ad2908d3402b7ff15beca5c0b0799295796c9f
Rishab2000 <rishabsachidanand@gmail.com>
2021-07-14 21:52:59 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <header><h1>Rishab Sachidanand</h1></header>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
         Button
       </button>
 
