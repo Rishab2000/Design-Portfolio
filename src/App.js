c76148b3781afdaf777cd4b8e690e8014e9a75ad
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-18 23:49:04 +0530

    Refactor code to create basic structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import Header from "./components/header";
 import LandingPage from "./content/LandingPage/landingPage";
-
 
 function App() {
   return (
     <div className="w-screen mt-16 bg-background-color">
       <Header />
-  
-      <Switch>
-<Route exact path="/" component={LandingPage}/>
 
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
       </Switch>
     </div>
   );
