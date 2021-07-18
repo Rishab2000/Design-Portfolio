843d38f4a743b3870c6426f860312eb57c0daee5
Rishab2000 <rishabsachidanand@gmail.com>
2021-07-18 11:17:45 +0530

    update mobile nav design

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import MobileNav from './mobileNav';
+import './header.css'
 
 const Header = () => {
     const [hidden, visible] = useState(false);
@@ -15,11 +16,18 @@ const Header = () => {
     }
 
     const buttonStack = hidden ? 'z-20' : 'z-50';
+    const hamburgerState = hidden ? 'open' : ' ';
     return (
         <div>
             <MobileNav status={hidden} />
             <header className="container flex items-center w-4/5 m-auto">
                 <h1 className="font-avenir-black text-base ">Rishab S</h1>
+                <div onClick={clickHandler} className={hamburgerState + " nav-icon3"}>
+                    <span className="nav-icon3-bar"></span>
+                    <span className="nav-icon3-bar"></span>
+                    <span className="nav-icon3-bar"></span>
+                    <span className="nav-icon3-bar"></span>
+                </div>
                 <button onClick={clickHandler} className={buttonStack + " ml-auto"}>test</button>
             </header>
 
--- src/components/header/mobileNav.js
+++ src/components/header/mobileNav.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 
 const MobileNav = (props) => {
-    const navClass = props.status ? 'opacity-0' : 'opacity-1';
+    const navClass = props.status ? 'opacity-1' : 'opacity-0';
     return (
 
-        <nav className={navClass + " transtion-opacity duration-300 ease-in flex flex-col gap-6 items-center absolute w-full h-full text-center pt-96 text-xl bg-background-color"}>
+        <nav className={navClass + " transtion-opacity duration-300 ease-in flex flex-col gap-6 items-center absolute w-full h-full text-center pt-96 text-xl font-avenir-bold bg-background-color"}>
             <a href='/' className="w-max hover:underline hover:text-link-hover-color">Work</a>
             <a href='/' className="w-max hover:underline hover:text-link-hover-color">Blog</a>
             <a href='/' className="w-max hover:underline hover:text-link-hover-color">About me</a>
