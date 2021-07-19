355951899ffb120b29aa4314134b8b80bb1f3f8c
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-19 16:26:41 +0530

    Add button to homescreen and improve hamburger behaviour

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,17 +1,14 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 
 const Button = (props) => {
   return (
-
+    <Link to={props.destination} className="hover:text-link-hover-color">
       <button className="text-avenir-book text-sm text-red hover:text-link-hover-color border-2 w-16 h-16 mt-8">
         More
       </button>
-
+    </Link>
   );
 };
 
 export default Button;
-
-
-{/* <Link to={props.destination} className="hover:text-link-hover-color"> */}
