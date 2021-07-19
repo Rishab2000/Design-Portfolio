4abfac54b845d4b7b70bca27c4209c25ea07f2a0
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-19 22:51:56 +0530

    added footer text

---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-4/5 mx-auto pt-8 border-t border-gray20">
+    
+     
+      <p className="font-avenir-medium mb-2 text-title-colour">
+        Don't be shy - drop me a line I'm looking forward to speaking to you!
+      </p>
+     <a className="font-avenir-medium text-gray40" href="mailto: rishabsachidanand@gmail.com" target="_blank" rel="noreferrer">rishabsachidanand@gmail.com</a>
+   
+    </footer>
+  );
+};
+
+export default Footer;
