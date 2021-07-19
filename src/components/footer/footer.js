26057e05c94f9dd59d26a0adbb0d5dc71c979b9e
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-19 22:55:55 +0530

    added footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         Don't be shy - drop me a line I'm looking forward to speaking to you!
       </p>
      <a className="font-avenir-medium text-gray40 hover:underline" href="mailto: rishabsachidanand@gmail.com" target="_blank" rel="noreferrer">rishabsachidanand@gmail.com</a>
-     <p className="text-avenir-book text-xs text-gray20 mt-8">© 2020, Rishab Sachidanand</p>
+     <p className="text-avenir-book text-xs text-gray20 mt-8">© 2021, Rishab Sachidanand</p>
    
     </footer>
   );
