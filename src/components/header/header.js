3b409304721f4ed3b3cfd8c286c707754beb28f7
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-27 22:19:33 +0530

    PROGRESS FINALLYYYY

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -24,7 +24,7 @@ const Header = (props) => {
         </div>
       </header>
       <div className={ headerState + " transtion-opacity duration-300 ease-in"}>
-        <p className= "font-avenir-medium pt-2 lg:text-3xl text-red">Human Centred Designer</p>
+        <p className= "font-avenir-medium pt-1 lg:pt-2 lg:text-3xl text-red">Human Centred Designer</p>
       </div>
       <IntroText className ={headerState+ " transtion-opacity duration-300 ease-in"} 
         text=" Rishab is bangalore based UX designer with a tendancy to design intuitive digital experiences in which every element serves a
