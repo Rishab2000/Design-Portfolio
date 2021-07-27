aad22b34b85a3244aa0c9cd1d5c811bef22d0b01
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-07-27 15:02:42 +0530

    nothing really matters

--- src/components/project-section-home/project-section-home-components/description.js
+++ src/components/project-section-home/project-section-home-components/description.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 const Description = (props) => {
   return (
-    <div className="w-10/12 lg:w-12/12   pt-10 lg:pt-5    pb-20 lg:pb-0">
-      <p className="font-avenir-medium text-lg mb-4 text-title-colour">{props.title}</p>
+    <div className="w-10/12 lg:w-12/12 mx-auto lg:mx-0  pt-5 lg:pt-5    pb-20 lg:pb-0">
+      <p className="font-avenir-medium text-lg lg:mb-4 text-title-colour">
+        {props.title}
+      </p>
       {/* <p className="font-avenir-book text-sm text-desc-colour">{props.desc}</p> */}
-      <div className="group-hover:text-red mt-8 transition-all duration-300 ease-in-out text-background-color">
-        <ArrowForwardIcon style={{fontSize:40}}/>
+      <div>
+        <ArrowForwardIcon className="group-hover:text-red ml-auto mt-4 lg:mt-8 transition-all duration-300 ease-in-out text-background-color" />
       </div>
     </div>
   );
--- src/content/LandingPage/landingPage.js
+++ src/content/LandingPage/landingPage.js
@@ -5,8 +5,12 @@ import ProjectSectionHome from "../../components/project-section-home";
 const LandingPage = (props) => {
   const LandingPageState = props.hidden ? "opacity-0" : " ";
   return (
-    <div className={LandingPageState + " mt-52 transtion-opacity duration-300 ease-in"}>
-      <div className= " lg:flex lg:mb-32 ">
+    <div
+      className={
+        LandingPageState + " lg:mt-52 transtion-opacity duration-300 ease-in"
+      }
+    >
+      <div className="flex flex-col items-center lg:flex-row lg:mb-32 ">
         <ProjectSectionHome
           className="group lg:w-6/12 "
           image={process.env.PUBLIC_URL + "/images/ibm.png"}
@@ -18,22 +22,22 @@ const LandingPage = (props) => {
         />
         <ProjectSectionHome
           image={process.env.PUBLIC_URL + "/images/sdplus.png"}
-          className="group lg:w-4/12 mt-44 lg:ml-auto"
+          className="group lg:w-4/12 lg:mt-44 lg:ml-auto"
           title="SD+ | UX Design Internship"
           // desc="Ideated and designed a B2B product to simplify sustainability. Outlined the experience road map of the
           // product to identify parts of the product needing engagement and improved them, adopting an agile model."
         />
       </div>
-      <div className="lg:w-12/12 lg:mb-32">
+      <div className="flex flex-col items-center lg:flex-row lg:mb-32">
         <ProjectSectionHome
-          className=" group lg:w-11/12 mx-auto"
+          className=" group lg:w-11/12 lg:mx-auto"
           image={process.env.PUBLIC_URL + "/images/chiral2.png"}
           title="Chiral Colonies - Climate change app | UX Case study "
           // desc=" A mobile based game that aims to spread awareness on climate change, geared towards young adults.
           // Working towards a substainable future by changing human behaviour."
         />
       </div>
-      <div className="flex lg:w-12/12 lg:mb-32 ">
+      <div className="flex flex-col items-center lg:flex-row lg:w-12/12 lg:mb-32 ">
         <ProjectSectionHome
           className="group lg:w-5/12 "
           image={
