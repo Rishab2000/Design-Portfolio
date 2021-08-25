30389cacdaf3bc946f11b1e7c79f46611fa19328
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-08-25 17:35:30 +0530

    update navigation

--- src/components/project-section-home/projectSectionHome.js
+++ src/components/project-section-home/projectSectionHome.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Image from "./project-section-home-components/image";
 import Description from "./project-section-home-components/description";
 
 const ProjectSectionHome = (props) => {
   return (
-    <a href={props.destination}  className={props.className + " w-screen h-auto flex flex-col"}>
+    <Link to={props.destination}  className={props.className + " w-screen h-auto flex flex-col"}>
     
         <Image hoverImage={props.hoverImage} image={props.image} />
         <Description
@@ -12,7 +13,7 @@ const ProjectSectionHome = (props) => {
           title={props.title}
           desc={props.desc}
         />
-    </a>
+    </Link>
   );
 };
 
