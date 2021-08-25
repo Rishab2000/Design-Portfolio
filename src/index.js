dc1d71fcf602904a266358872e81e26c97a97896
Rishab Sachidanand <rishabsachidanand@MacBook-Pro.local>
2021-08-25 17:45:06 +0530

    fix sticky scrol on new page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ScrollToTop from "./components/scroll-to-top"
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop/>
     <App />
   </Router>,
   document.getElementById("root")
