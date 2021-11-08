c971db5ab7c2d27efee7394473c68e4cefb04ac9
Rishab2000 <rishabsachidanand@gmail.com>
2021-11-08 13:52:05 +0530

    update intro text

--- src/components/project-section-home/project-section-home-components/introText.js
+++ src/components/project-section-home/project-section-home-components/introText.js
@@ -3,7 +3,7 @@ import React from "react";
 const IntroText = (props) => {
   return (
     <div className={ props.className + "  pt-16 pb-12 md:pt-24 md:pb-56 md:pr-30 xl:pr-0 "}>
-      <p className="font-avenir-book sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-title-colour">
+      <p className="font-avenir-book text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-title-colour">
         {props.textHeading}
       </p>
       <p className="font-avenir-book mt-8 sm:text-base md:text-md lg:text-xl lg:w-9/12 xl:text-xl 2xl:text-2xl text-desc-colour">{props.caption}</p>
